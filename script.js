0426b388f19aecd5fea6ec4ef68c8ac26b7b0577
AsmitBro <asmitkhanal335@gmail.com>
2024-04-17 20:11:58 +0545

    design complete and js logic remaining

--- script.js
+++ script.js
@@ -1,7 +1,43 @@
-function handleAddBtn() {
-    setTimeout(() => {
-        console.log("clicked");
+let inputofContent = document.getElementById("input")
+let listValue = document.querySelector(".listContainer")
 
-    }, 2000);
-    setInterval(1000, "btnClicked")
+
+function showToast(message) {
+    const toast = document.createElement("div");
+    toast.classList.add("toast");
+    toast.textContent = message;
+
+    const toastcontainer = document.getElementById("toastcontainer");
+    toastcontainer.appendChild(toast);
+
+    setTimeout(() => {
+        toast.classList.add("show");
+    }, 100);
+
+    setTimeout(() => {
+        toast.classList.remove("show");
+        setTimeout(() => {
+            toastcontainer.removeChild(toast);
+        }, 300);
+    }, 3000);
+}
+
+function handleAddBtn() {
+
+    if (!inputofContent.value) {
+        showToast("Please enter Todo Content.");
+        return;
+    }
+
+    let li = document.createElement("li")
+    li.innerHTML = inputofContent.value
+    listValue.appendChild(li)
+
+    let span = document.createElement("span")
+    span.innerHTML = "\u00d7"
+
+    li.appendChild(span)
+
+    inputofContent.value = ""
+    console.log("clicked");
 }
